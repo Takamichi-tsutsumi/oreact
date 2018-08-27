7d5bfa0d18e3b04b037cfb6751e5e3423fce89e2
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-08-27 11:20:04 +0900

    Test Entry point

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-import hello from './hello'
+import { h } from './vdom'
+import render from './render'
 
-function main() {
-  return hello()
+const main = () => {
+  const app = h('div', { className: 'app' }, [
+    h('h1', { className: 'page-title' }, 'Title'),
+    h('p', { className: 'page-content' }, 'paragraph')
+  ])
+  const root = document.getElementById('root')
+  if (root) {
+    render(app, root)
+  }
 }
 
 main()
