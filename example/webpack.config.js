fa0e8ee118cd7292b73e3e6d831810753271cd4a
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2018-11-13 19:38:24 +0900

    extract example directory

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html"
+      template: "./example/index.html"
     }),
     new CleanWebpackPlugin(["dist"])
   ]
